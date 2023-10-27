74fbcd8dc955305cc878a7ebfb6dc1de3acd5e68
jewel-snake <a.usatyuk03@proton.me>
2023-10-27 21:31:47 +0300

    initial commit

---
+++ initdb.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE pharmchan;
+CREATE USER pharmchandjango WITH ENCRYPTED PASSWORD 'pharmchan2024';
+ALTER ROLE pharmchandjango SET client_encoding TO 'utf8';
+ALTER ROLE pharmchandjango SET default_transaction_isolation TO 'read committed';
+ALTER ROLE pharmchandjango SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE pharmchan TO pharmchandjango;
