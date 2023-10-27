ad73351c4cbbbbe2c69e0ab3b7542408ae74fb19
jewel-snake <a.usatyuk03@proton.me>
2023-10-27 21:38:26 +0300

    postgre is now default db

--- pharmchan/settings.py
+++ pharmchan/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pharmchan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgesql_psycopg2',
+        'NAME': 'pharmchan',
+        'PASSWORD': 'pharmchan2024',
+        'USER': 'pharmchandjango',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
