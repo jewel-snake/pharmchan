f1480b52ee960f77b6ecd1cd78ef54c9aaa61307
jewel-snake <a.usatyuk03@proton.me>
2023-11-07 22:08:53 +0300

    create schema for user

--- initdb.sql
+++ initdb.sql
@@ -4,3 +4,6 @@ ALTER ROLE pharmchandjango SET client_encoding TO 'utf8';
 ALTER ROLE pharmchandjango SET default_transaction_isolation TO 'read committed';
 ALTER ROLE pharmchandjango SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE pharmchan TO pharmchandjango;
+CREATE SCHEMA pharmchandjango;
+GRANT USAGE ON SCHEMA pharmchandjango TO pharmchandjango;
+GRANT CREATE ON SCHEMA pharmchandjango TO pharmchandjango;
