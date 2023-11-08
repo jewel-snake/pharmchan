ab3b627f4ab35f9eb673cd69d98874c5dcb6d9f0
jewel-snake <a.usatyuk03@proton.me>
2023-11-08 20:55:48 +0300

    created compmodel app; initial

---
+++ compmodel/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class CompModel(models.Model):
+    """
+        Compartment model ehehehe
+    содержит неявно объявленное поле id - primary key, это внутрений механизм джанго
+    author foreign key user - вторичный ключ, ссылающийся на сущность пользователя 
+    django.contrib.auth.models.User.
+    body text - безразмерное текстовое поле, хранящее модель в промежуточном
+    представлении в сериализованной форме. целостность модели проверяется
+    при первичном создании и каждой десериализации.
+    """
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    body = models.TextField
