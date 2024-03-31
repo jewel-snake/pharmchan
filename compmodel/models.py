e0019b0f6976397fa21850d068aebf7deea94a9d
jewel-snake <a.usatyuk03@proton.me>
2024-03-31 21:27:09 +0300

    remove name for solution

--- compmodel/models.py
+++ compmodel/models.py
@@ -12,5 +12,20 @@ class CompModel(models.Model):
     при первичном создании и каждой десериализации.
     """
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    # in here MESystem.__dict__ goes
+    body = models.JSONField(null=False,blank=False) 
+    # django 4.2.7 does not have JSONField listed in documentation,
+    # however it does mentions HStoreField that exhibits exact same behaviour
+    # proposal: substitute body with arrays of charfields
+    name = models.CharField(max_length=50,null=False,blank=True)
+
+class Solution(models.Model):
+    """
+        Класс решения модели. содержит внешний ключ, указывающий на модель, при удалении модели
+    удаляется и решение. Поле body представляет собой JSON как и модель, но без null значений.
+    """
+    model = models.ForeignKey(CompModel,on_delete=models.CASCADE,null=False)
     body = models.JSONField(null=False,blank=False)
-    name = models.CharField(50,null=False,blank=True)
+
+#class DataSet(models.Model):
+    
