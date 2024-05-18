4cce29cfa2910d07292ca67bca1bb51814bedf53
jewel-snake <a.usatyuk03@proton.me>
2024-05-18 22:07:42 +0300

    модуль расчётов

---
+++ engine/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+#class FutureSolution(models.Model):
+#    owner = models.ForeignKey(User,null=False)
+#    model = models.ForeignKey(CompModel,null=False)
+#    dataset = models.ForeignKey()
